d0d48be5c076fa4941edfdd89a5d5735312a147b
小田尚未 <odanaomi@oda3noMacBook-Air.local>
2020-05-17 22:51:02 +0900

    質問5と6に回答

--- task.rb
+++ task.rb
@@ -37,14 +37,16 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map { |number| number * 10 }
+  p numbers2
 end
 
 def q7
